58f2dcf4a02d767969c3d57925d88db9545c0062
Ricardo Antunes <riscado.antunes@gmail.com>
2021-01-29 15:56:43 +0000

    Finished basic text bullet

--- levels/memory/text_bullet.gd
+++ levels/memory/text_bullet.gd
@@ -1,14 +1,13 @@
 extends Node2D
 
-
 export var text = ""
 var progress = 0
 var original_text
 
 func _ready():
-	original_text  = $Text.bbcode_text
-	$Text.bbcode_text = original_text.replace("{text}", text);
-
+	$Text/Begin.text = ""
+	$Text/End.text = text
+	
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if 65 <= event.scancode and event.scancode <= 90: # A to Z
@@ -22,7 +21,6 @@ func enter_character(chr):
 	if progress == len(text):
 		get_parent().remove_child(self)
 	else:
-		$Text.bbcode_text = original_text.replace(
-			"{text}",
-			"[size=14]%s[/size][size=10]%s[/size]" % [text.substr(0, progress), text.substr(progress)]
-		);
+		$Text/Begin.text = text.substr(0, progress)
+		$Text/End.text = text.substr(progress)
+		
