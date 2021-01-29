b900be6d3e229fc812308a8186a422ee47167571
Luís Fonseca <luis.h.fonseca@tecnico.ulisboa.pt>
2021-01-29 23:28:44 +0000

    custom text

--- levels/memory/memory.gd
+++ levels/memory/memory.gd
@@ -3,21 +3,38 @@ extends Node2D
 const TextBullet = preload("res://levels/memory/text_bullet.tscn")
 
 var rng = RandomNumberGenerator.new()
+var words
+
 onready var viewport_size = get_viewport().size
 
-# export(float, 180) var spawn_angle
+export(String, MULTILINE) var paragraph
 export(float) var average_bullet_velocity
 export(float, 0, 1) var velocity_bound
 
 func _ready():
+    rng.randomize()
+    
     $Player.position.x = viewport_size.x / 2
     $Player.position.y = viewport_size.y - $Player.get_rect().size.y / 2
     
-    rng.randomize()
-
+    for illegal in [".", ","]:
+        paragraph = paragraph.replace(illegal, "")
+    words = paragraph.split(" ", false)
+    for i in range(len(words)):
+        words[i] = words[i].strip_edges()
 
 func _on_Timer_timeout():
+    if len(words) == 0:
+        if $TextBullets.get_child_count() == 0:
+            print_debug('we won!')
+            queue_free()
+        return
+        
     var text_bullet = TextBullet.instance()
+    
+    var i = rng.randi_range(0, len(words) - 1)
+    text_bullet.text = words[i]
+    words.remove(i)
     
     text_bullet.position = Vector2(rng.randi_range(0, viewport_size.x), 0)
     
