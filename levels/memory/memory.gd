84e7b5bedbd0e625ab319a4826b75acfa2d595b5
Luís Fonseca <luis.h.fonseca@tecnico.ulisboa.pt>
2021-01-29 20:21:18 +0000

    spawn text bullets in random position

--- levels/memory/memory.gd
+++ levels/memory/memory.gd
@@ -1,10 +1,27 @@
 extends Node2D
 
+const TextBullet = preload("res://levels/memory/text_bullet.tscn")
+
+var rng = RandomNumberGenerator.new()
+onready var viewport_size = get_viewport().size
+
+# export(float, 180) var spawn_angle
+export(float) var average_bullet_velocity
+export(float, 0, 1) var velocity_bound
+
 func _ready():
-	$Player.position.x = get_viewport_rect().size.x / 2
-	$Player.position.y = get_viewport_rect().size.y - $Player.get_rect().size.y / 2
+    $Player.position.x = viewport_size.x / 2
+    $Player.position.y = viewport_size.y - $Player.get_rect().size.y / 2
+    
+    rng.randomize()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
+func _on_Timer_timeout():
+    var text_bullet = TextBullet.instance()
+    
+    text_bullet.position = Vector2(rng.randi_range(0, viewport_size.x), 0)
+    
+    var direction = text_bullet.position.direction_to($Player.position)
+    var scalar = clamp(rng.randfn(1), 1 - velocity_bound, 1 + velocity_bound)
+    text_bullet.velocity = direction * average_bullet_velocity * scalar
+    $TextBullets.add_child(text_bullet)
--- levels/memory/text_bullet.gd
+++ levels/memory/text_bullet.gd
@@ -4,25 +4,28 @@ export var text = ""
 var progress = 0
 var original_text
 
-export(Vector3) var velocity;
+export(Vector2) var velocity;
 
 func _ready():
-	$Text/Begin.text = ""
-	$Text/End.text = text
-	
+    $Text/Begin.text = ""
+    $Text/End.text = text
+    
 func _input(event):
-	if event is InputEventKey and event.pressed:
-		if 65 <= event.scancode and event.scancode <= 90: # A to Z
-			enter_character(char(event.scancode))
+    if event is InputEventKey and event.pressed:
+        if 65 <= event.scancode and event.scancode <= 90: # A to Z
+            enter_character(char(event.scancode))
+            
+func _process(delta):
+    position += velocity * delta
 
 func enter_character(chr):
-	if text[progress].to_upper() != chr:
-		return
-	
-	progress += 1
-	if progress == len(text):
-		get_parent().remove_child(self)
-	else:
-		$Text/Begin.text = text.substr(0, progress)
-		$Text/End.text = text.substr(progress)
-		
+    if text[progress].to_upper() != chr:
+        return
+    
+    progress += 1
+    if progress == len(text):
+        get_parent().remove_child(self)
+    else:
+        $Text/Begin.text = text.substr(0, progress)
+        $Text/End.text = text.substr(progress)
+        
