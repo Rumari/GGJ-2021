4fe58ad347918c2c719d72a10571717ed98e1067
Mateus Pinho <mateusleitepinho@gmail.com>
2021-01-30 12:07:10 +0000

    Changed song

--- Global.gd
+++ Global.gd
@@ -8,32 +8,32 @@ var destroy_on_load = []
 
 func _ready():
 
-    current_scene = root.get_child(root.get_child_count() - 1)
-    
+	current_scene = root.get_child(root.get_child_count() - 1)
+	
 func goto_scene_from_path(path):
-    goto_scene(ResourceLoader.load(path))
-    
+	goto_scene(ResourceLoader.load(path))
+	
 func save_old_and_goto_scene(scene):
-    root.remove_child(current_scene)
-    old_scene = current_scene
-    call_deferred("_deferred_goto_scene", scene)
-    
+	root.remove_child(current_scene)
+	old_scene = current_scene
+	call_deferred("_deferred_goto_scene", scene)
+	
 func goto_old():
-    goto_scene(old_scene)
-    
+	goto_scene(old_scene)
+	
 func goto_old_and_destroy(node):
-    destroy_on_load.append(node)
-    goto_old()
-    
+	destroy_on_load.append(node)
+	goto_old()
+	
 func goto_scene(scene):
-    root.remove_child(current_scene)
-    current_scene.queue_free()
-    call_deferred("_deferred_goto_scene", scene)
+	root.remove_child(current_scene)
+	current_scene.queue_free()
+	call_deferred("_deferred_goto_scene", scene)
 
 func _deferred_goto_scene(scene):
-    current_scene = scene
-    get_tree().get_root().add_child(current_scene)
-    get_tree().set_current_scene(current_scene)
+	current_scene = scene
+	get_tree().get_root().add_child(current_scene)
+	get_tree().set_current_scene(current_scene)
 
-    for np in destroy_on_load:
-        get_node(np).queue_free()
+	for np in destroy_on_load:
+		get_node(np).queue_free()
--- levels/exploration/interactions/memory.gd
+++ levels/exploration/interactions/memory.gd
@@ -5,7 +5,7 @@ const scene = preload("res://levels/memory/memory.tscn")
 export(String, MULTILINE) var memory_text
 
 func run():
-    var s = scene.instance()
-    s.paragraph = memory_text
-    s.destroy_on_win = get_parent().get_path()
-    Global.save_old_and_goto_scene(s)
+	var s = scene.instance()
+	s.paragraph = memory_text
+	s.destroy_on_win = get_parent().get_path()
+	Global.save_old_and_goto_scene(s)
