adcb7537fdf1b02bb0c9c846baf4380399030b5f
Mateus Pinho <mateusleitepinho@gmail.com>
2021-01-30 11:35:40 +0000

    finally added E

--- levels/exploration/interactable.gd
+++ levels/exploration/interactable.gd
@@ -3,9 +3,19 @@ extends Area
 export(float) var rot_speed
 export(NodePath) var interaction
 
+func _enter_tree():
+	get_parent().get_node("Label").hide()
+	
 func _process(delta):
-    $Sprite.rotate_y(delta * rot_speed)
+	$Sprite.rotate_y(delta * rot_speed)
+	show()
 
 func _input(event):
-    if event.is_action_pressed("confirm") && len(get_overlapping_bodies()) > 0:
-        get_node(interaction).run()
+	if event.is_action_pressed("confirm") && len(get_overlapping_bodies()) > 0:
+		get_node(interaction).run()
+
+func show():
+	if len(get_overlapping_bodies()) > 0:
+		get_parent().get_node("Label").show()
+	else:
+		get_parent().get_node("Label").hide()
