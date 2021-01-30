21666742511a8c844eff9de0faed9173f2af6a5d
Ricardo Antunes <riscado.antunes@gmail.com>
2021-01-30 11:04:04 +0000

    Enhanced shmup

--- levels/memory/memory.gd
+++ levels/memory/memory.gd
@@ -4,6 +4,8 @@ const TextBullet = preload("res://levels/memory/text_bullet.tscn")
 
 var rng = RandomNumberGenerator.new()
 var words
+var current_first_characters = []
+var targeted_bullet = null
 
 onready var viewport_size = get_viewport().size
 
@@ -12,33 +14,58 @@ export(float) var average_bullet_velocity
 export(float, 0, 1) var velocity_bound
 
 func _ready():
-    rng.randomize()
-    
-    $Player.position.x = viewport_size.x / 2
-    $Player.position.y = viewport_size.y - $Player.get_rect().size.y / 2
-    
-    for illegal in [".", ","]:
-        paragraph = paragraph.replace(illegal, "")
-    words = paragraph.split(" ", false)
-    for i in range(len(words)):
-        words[i] = words[i].strip_edges()
+	rng.randomize()
+	
+	$Player.position.x = viewport_size.x / 2
+	$Player.position.y = viewport_size.y - $Player.get_rect().size.y / 2
+	
+	for illegal in [".", ","]:
+		paragraph = paragraph.replace(illegal, "")
+	words = paragraph.split(" ", false)
+	for i in range(len(words)):
+		words[i] = words[i].strip_edges().to_upper()
+		
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if 65 <= event.scancode and event.scancode <= 90: # A to Z
+			var chr = char(event.scancode)
+			if targeted_bullet == null:
+				for bullet in $TextBullets.get_children():
+					match bullet.enter_character(chr):
+						0:
+							continue
+						1:
+							bullet.select()
+							targeted_bullet = bullet
+							current_first_characters.erase(chr)
+							break
+						2:
+							current_first_characters.erase(chr)
+							break
+			elif targeted_bullet.enter_character(chr) == 2:
+				targeted_bullet = null
 
+func spawn_bullet(word):
+	current_first_characters.append(word[0])
+	
+	var bullet = TextBullet.instance()
+	bullet.text = word
+	bullet.position = Vector2(rng.randi_range(0, viewport_size.x), 0)
+	
+	var direction = bullet.position.direction_to($Player.position)
+	var scalar = clamp(rng.randfn(1), 1 - velocity_bound, 1 + velocity_bound)
+	bullet.velocity = direction * average_bullet_velocity * scalar
+	$TextBullets.add_child(bullet)
+				
 func _on_Timer_timeout():
-    if len(words) == 0:
-        if $TextBullets.get_child_count() == 0:
-            print_debug('we won!')
-            queue_free()
-        return
-        
-    var text_bullet = TextBullet.instance()
-    
-    var i = rng.randi_range(0, len(words) - 1)
-    text_bullet.text = words[i]
-    words.remove(i)
-    
-    text_bullet.position = Vector2(rng.randi_range(0, viewport_size.x), 0)
-    
-    var direction = text_bullet.position.direction_to($Player.position)
-    var scalar = clamp(rng.randfn(1), 1 - velocity_bound, 1 + velocity_bound)
-    text_bullet.velocity = direction * average_bullet_velocity * scalar
-    $TextBullets.add_child(text_bullet)
+	if len(words) == 0:
+		if $TextBullets.get_child_count() == 0:
+			print_debug('we won!')
+			queue_free()
+		return
+		
+	for i in range(len(words)):
+		if !current_first_characters.has(words[i][0]):
+			spawn_bullet(words[i])
+			words.remove(i)
+			break
--- levels/memory/text_bullet.gd
+++ levels/memory/text_bullet.gd
@@ -3,32 +3,37 @@ extends Node2D
 export var text = ""
 var progress = 0
 var original_text
+var selected = false
+var offset
 
-export(Vector2) var velocity;
+export(Vector2) var velocity
+export(Color) var selected_color
 
 func _ready():
-    $Text/Begin.text = ""
-    $Text/End.text = text
-    
-func _input(event):
-    if event is InputEventKey and event.pressed:
-        if 65 <= event.scancode and event.scancode <= 90: # A to Z
-            enter_character(char(event.scancode))
-            
-func _process(delta):
-    position += velocity * delta
+	$Text/Begin.text = ""
+	$Text/End.text = text
+	
+func _physics_process(delta):
+	position += velocity * delta
+
+func select():
+	selected = true
+	$Text/Begin.add_color_override("font_color", selected_color) 
+	$Text/End.add_color_override("font_color", selected_color) 
 
 func enter_character(chr):
-    if text[progress].to_upper() != chr:
-        return
-    
-    progress += 1
-    if progress == len(text):
-        get_parent().remove_child(self)
-    else:
-        $Text/Begin.text = text.substr(0, progress)
-        $Text/End.text = text.substr(progress)
-        
+	if text[progress].to_upper() != chr:
+		return 0
+	
+	progress += 1
+	if progress == len(text):
+		queue_free()
+		return 2
+	else:
+		$Text/Begin.text = text.substr(0, progress)
+		$Text/End.text = text.substr(progress)
+		return 1
+		
 func _on_area_entered(area):
-    area.damage()
-    queue_free()
+	area.damage()
+	queue_free()
