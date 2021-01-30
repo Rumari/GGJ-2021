43557f543efe2b2fe9515a81c25800bdf0c997a2
Ricardo Antunes <riscado.antunes@gmail.com>
2021-01-30 11:10:29 +0000

    Merged

--- levels/exploration/camera.gd
+++ levels/exploration/camera.gd
@@ -5,14 +5,14 @@ onready var max_distance = translation.length()
 onready var height = translation.y
 
 func _physics_process(delta):
-    var space_state = get_world().direct_space_state
-    var hit = space_state.intersect_ray(
-        get_parent().to_global(Vector3.ZERO),
-        get_parent().to_global(dir * max_distance)
-    )
-    
-    var distance = max_distance
-    if hit:
-        distance = (hit.position - get_parent().global_transform.origin).length()
-    translation = dir * distance
-    translation.y = height
+	var space_state = get_world().direct_space_state
+	var hit = space_state.intersect_ray(
+		get_parent().to_global(Vector3.ZERO),
+		get_parent().to_global(dir * max_distance)
+	)
+	
+	var distance = max_distance
+	if hit:
+		distance = (hit.position - get_parent().global_transform.origin).length()
+	translation = dir * distance
+	translation.y = height
